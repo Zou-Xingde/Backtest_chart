66af3a63c1a0e049fac89233a52c3322a1e1b2f1
XDZOU <ahy05132@gmail.com>
2024-11-22 05:29:35 +0800

    部署-49

--- EX-01.py
+++ EX-01.py
@@ -53,7 +53,7 @@ def update_output(contents, filename):
             import io
             import base64
             decoded = base64.b64decode(content_string)
-            df = pd.read_excel('EX.xlsx')
+            df = pd.read_excel(decoded)
             # 返回表格視圖
             return dash_table.DataTable(
                 data=df.to_dict("records"),
